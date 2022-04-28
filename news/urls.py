349b613ba88050375bd22b65883b565a3e13b2c2
Pratik Vinayak More <C2K20106742@pictsctr.onmicrosoft.com>
2022-04-29 02:28:35 +0530

    wrote functions in views.py file for creating scrapper for respective categories and linked those files in urls.py

--- news/urls.py
+++ news/urls.py
@@ -7,9 +7,13 @@ from news import views
 urlpatterns = [
     
     path('', views.home,name="home" ),
-    path('categories',views.categories,name="categories" ),
-    path('about',views.aboutx,name="about" )
-    
+    path('main',views.main,name="main" ),
+    path('about',views.aboutx,name="about" ),
+    path('sports',views.categories_sp,name="categories_sp"),
+    path('politics',views.categories_po,name="categories_po"),
+    path('education',views.categories_edu, name="categories_edu"),
+    path('technology',views.categories_tec, name="categories_tec"),
+    path('weather',views.categories_wea, name="categories_wea"),
     
     ]
     
