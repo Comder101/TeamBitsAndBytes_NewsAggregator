9b6acf59ada6d2834121eda53f26ec871707070e
Pratik Vinayak More <86362195+ImAnonymous1@users.noreply.github.com>
2022-04-29 19:33:36 +0530

    Update settings.py

--- TeamBitsAndBytes_NewsAggregator/settings.py
+++ TeamBitsAndBytes_NewsAggregator/settings.py
@@ -8,10 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xjfk(llan%=4kq-x3$%i#*$o0tn!3orlgs8q(-irtsyoj5c-@r'
 
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
