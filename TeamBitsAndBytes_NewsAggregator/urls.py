1294b9ec9df3aa2ad5b2a4189decaf6273152bfa
Pratik Vinayak More <C2K20106742@pictsctr.onmicrosoft.com>
2022-05-02 04:58:18 +0530

    hell

--- TeamBitsAndBytes_NewsAggregator/urls.py
+++ TeamBitsAndBytes_NewsAggregator/urls.py
@@ -5,8 +5,7 @@ from news import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('news.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    
     
     ]
     
