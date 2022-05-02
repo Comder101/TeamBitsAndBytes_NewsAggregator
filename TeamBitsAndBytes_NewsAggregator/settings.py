1c87b0ef966a09d3812906e8d035690ab5cd650e
Pratik Vinayak More <C2K20106742@pictsctr.onmicrosoft.com>
2022-05-02 08:39:17 +0530

    static files

--- TeamBitsAndBytes_NewsAggregator/settings.py
+++ TeamBitsAndBytes_NewsAggregator/settings.py
@@ -94,7 +94,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
      os.path.join(BASE_DIR, 'static')
 ]
-
+STATIC_ROOT = [
+     os.path.join(BASE_DIR, 'staticfiles')
+]
 
 
 MEDIA_URL = "/images/"
