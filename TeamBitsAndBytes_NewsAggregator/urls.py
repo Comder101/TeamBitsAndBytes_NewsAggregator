49138deb5293e8b6ac4355e296717fcc0565aa25
Pratik Vinayak More <C2K20106742@pictsctr.onmicrosoft.com>
2022-05-02 05:58:32 +0530

    staticfiles error - root not found

--- TeamBitsAndBytes_NewsAggregator/urls.py
+++ TeamBitsAndBytes_NewsAggregator/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
 from news import views
+from django.views.static import serve
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
